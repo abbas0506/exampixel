8de5810a6a902fe5c669220f61e8eb27e855e4b7
mianazeemdaula <mianazeemdaula@gmail.com>
2024-09-13 09:19:43 +0500

    tags

--- resources/views/layouts/basic.blade.php
+++ resources/views/layouts/basic.blade.php
@@ -67,6 +67,19 @@
             style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
 
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-BN651Z3VC4"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-BN651Z3VC4');
+    </script>
+
     @yield('header')
     @yield('sidebar')
 
